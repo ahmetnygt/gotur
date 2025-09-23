f193fa6376eeeb6a69a7b8129cc4cffb071142fc
Ahmet Neyiğit <ahmetnygt@hotmail.com>
2025-09-24 02:02:39 +0300

    ahmetnygt

--- utilities/goturDb.js
+++ utilities/goturDb.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require("sequelize");
 const PlaceFactory = require("../models/placeModel");
 const FirmFactory = require("../models/firmModel");
+const UserFactory = require("../models/userModel");
 
 const goturDB = new Sequelize("gotur", "doadmin", "AVNS_rfP7FS1Hdg-KSHpn02u", {
     host: "dbaas-db-5929049-do-user-22627641-0.g.db.ondigitalocean.com",
@@ -12,10 +13,11 @@ const goturDB = new Sequelize("gotur", "doadmin", "AVNS_rfP7FS1Hdg-KSHpn02u", {
 function initGoturModels() {
     // const User = UserFactory(goturDb);
     const Place = PlaceFactory(goturDB);
+    const User = UserFactory(goturDB);
     const Firm = FirmFactory(goturDB);
     goturDB.sync();
 
-    return { Place, Firm };
+    return { Place, Firm, User };
 }
 
 module.exports = { goturDB, initGoturModels };
