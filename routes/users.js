3126f952d59b5d525b975007e2b92f7bcd26f67e
Ahmet Neyiğit <98274533+ahmetnygt@users.noreply.github.com>
2025-09-24 16:22:53 +0300

    Add personal information management page

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.get("/", userController.myAccount);
-router.get("/personal-information", userController.myAccount);
+router.get("/", userController.redirectToPersonalInformation);
+router.get("/personal-information", userController.personalInformation);
+router.post("/personal-information", userController.updatePersonalInformation);
+router.post(
+  "/personal-information/change-password",
+  userController.changePassword,
+);
 router.get("/cards", userController.myAccount);
 router.get("/passengers", userController.myAccount);
 router.post("/login", userController.login);
